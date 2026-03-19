5787ff0f7c7075c9d3838fc64b00f2f2d8793016
Juan ojeda <puanse8@gmail.com>
2026-03-19 11:23:35 -0300

    Add files via upload

---
+++ Tp2.cs
@@ -0,0 +1,170 @@
+﻿int a = 25;
+int b = 30;
+
+Console.WriteLine("1) 25 > 30?");
+Console.WriteLine("");
+
+if (a > b)
+{
+    Console.WriteLine("Si, es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int c = 14;
+int d = 14;
+
+Console.WriteLine("2) 14 <= 14?");
+Console.WriteLine("");
+
+if (c <= d)
+{
+    Console.WriteLine("Si, es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es verdadero");
+    Console.WriteLine("");
+}
+
+int e = 0;
+int f = 0;
+
+Console.WriteLine("3) 0 == 0 ?");
+Console.WriteLine("");
+
+if (e == f)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int g = 10;
+int h = 10;
+
+Console.WriteLine("4) 10 != 10 ?");
+Console.WriteLine("");
+
+if (g != h)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int i = 8;
+int j = 3;
+
+Console.WriteLine("5) 8 >= 3  ?");
+Console.WriteLine("");
+
+if (i >= j)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int l = 15;
+int m = 15;
+
+Console.WriteLine("6) 15 < 15 ?");
+Console.WriteLine("");
+
+if (l < m)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int n = 100;
+int o = 50;
+
+Console.WriteLine("7) 100 != 50?");
+Console.WriteLine("");
+
+if (n != o)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int p = 7;
+float q = 7.0f;
+
+Console.WriteLine("8) 7 == 7.0 ?");
+Console.WriteLine("");
+
+if (p == q)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+
+float r = 0.5f;
+int s = 0;
+
+Console.WriteLine("9) 0.5 > 0 ?");
+Console.WriteLine("");
+
+if (r > s)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
+
+int t = -5;
+int w = -2;
+
+Console.WriteLine("10) -5 < -2 ?");
+Console.WriteLine("");
+
+if (t < w)
+{
+    Console.WriteLine("si , es verdadero");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("No, es falso");
+    Console.WriteLine("");
+}
