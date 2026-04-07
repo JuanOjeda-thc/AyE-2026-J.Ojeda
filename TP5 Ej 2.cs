c0e7611dbdd5e9ba0585aa59a259161a10c9b1a3
Juan ojeda <puanse8@gmail.com>
2026-04-07 11:32:27 -0300

    Add files via upload

---
+++ TP5 Ej 2.cs
@@ -0,0 +1,10 @@
+﻿//Ejercicio 2
+
+int contador = 5;
+while (contador >= 1)
+{
+    Console.WriteLine(contador);
+    contador--;
+}
+Console.WriteLine("¡Listo para despegar!");
+Console.WriteLine("¡Despegue!");
