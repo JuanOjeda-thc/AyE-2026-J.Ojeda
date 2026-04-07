87d1f883e207dd66a58283001efe4a12cb7a0863
Juan ojeda <puanse8@gmail.com>
2026-04-07 11:18:58 -0300

    Add files via upload

---
+++ TP4.cs
@@ -0,0 +1,46 @@
+﻿// Ejercicio 1
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+// Ejercicio 2
+Console.WriteLine("");
+Console.WriteLine("EJERCICIO 2");
+Console.WriteLine("");
+for (int a = 1; a <= 5; a++)
+{
+    Console.WriteLine("¡Hola Mundo!");
+}
+
+// Ejercicio 3
+Console.WriteLine("");
+Console.WriteLine("EJERCICIO 3");
+Console.WriteLine("");
+for (int b = 2; b <= 20; b = b + 2)
+{
+    Console.WriteLine(b);
+}
+
+// Ejercicio 4
+Console.WriteLine("");
+Console.WriteLine("EJERCICIO 4");
+Console.WriteLine("");
+for (int c = 7; c <= 70; c = c + 7)
+{
+    Console.WriteLine(c);
+}
+
+// Ejercicio 5
+Console.WriteLine("");
+Console.WriteLine("EJERCICIO 5");
+Console.WriteLine("");
+int acum;
+for (int d = 1; d <= 5; d++)
+{
+    acum = d;
+    acum += d;
+    Console.WriteLine(acum);
+}
