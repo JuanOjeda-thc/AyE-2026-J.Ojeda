21d3d79ce20a8b3861e587811a318f7001bb9543
Juan ojeda <puanse8@gmail.com>
2026-04-07 11:27:20 -0300

    Add files via upload

---
+++ TP5 Ej 1.cs
@@ -0,0 +1,16 @@
+﻿//Ejercicio 1
+int codigo = 1948;
+
+while (codigo != 1948)
+{
+    Console.WriteLine("Ingrese el código de acceso:");
+    codigo = int.Parse(Console.ReadLine());
+
+    if (codigo != 1948)
+    {
+        Console.WriteLine("Código incorrecto. Intente nuevamente.");
+    }
+    else {
+        Console.WriteLine("Código correcto. Acceso concedido.");
+    }
+}
