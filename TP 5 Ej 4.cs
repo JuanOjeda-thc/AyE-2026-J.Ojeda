baa451a9b1c9d62bd0a4133e0384ba3ad4c53132
Juan ojeda <puanse8@gmail.com>
2026-04-07 11:47:56 -0300

    Add files via upload

---
+++ TP 5 Ej 4.cs
@@ -0,0 +1,16 @@
+﻿// Ejercicio 4
+int suma = 0;
+int numero;
+
+Console.WriteLine("Ingrese un número");
+Console.WriteLine("Ingrese el número 0 para finalizar");
+numero = Convert.ToInt32(Console.ReadLine());
+suma = suma + numero;
+while (numero != 0)
+{
+    Console.WriteLine(numero);
+    numero = Convert.ToInt32(Console.ReadLine());
+    suma = suma + numero;
+}
+Console.WriteLine("La suma total es: ");
+Console.Write(suma);
