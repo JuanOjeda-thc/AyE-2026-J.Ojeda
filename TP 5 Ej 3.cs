e878ca910c92414498844a5c36bc2b2903be4fa1
Juan ojeda <puanse8@gmail.com>
2026-04-07 11:41:56 -0300

    Add files via upload

---
+++ TP 5 Ej 3.cs
@@ -0,0 +1,19 @@
+﻿//Ejercicio 3
+
+int secreto = 7;
+
+Console.WriteLine("Adivina el número secreto entre 1 y 10");
+while (secreto != 7) ;
+{
+    Console.WriteLine("Escriba el numero secreto!.");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (secreto == 7) ;
+    {
+        Console.WriteLine("¡Felicidades! Has adivinado el número secreto.");
+    }
+    else
+    {
+        Console.WriteLine("¡Inténtalo de nuevo!");
+    }
+}
