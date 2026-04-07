04cc13a7c68db5688fedb673ac150ce6ddb5c531
Juan ojeda <puanse8@gmail.com>
2026-04-07 13:10:22 -0300

    Add files via upload

---
+++ Simulacro de evaluacion.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("ingrese su promedio actual: ");
+float promedio = float.Parse(Console.ReadLine());
+Console.WriteLine("ingrese la distancia en kilometros de su casa a la universidad: ");
+int distancia = int.Parse(Console.ReadLine());
+
+bool DeterminarBeca(float promedio, int distancia)
+{
+    if (promedio >= 8.5 || distancia > 50)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool resultado = DeterminarBeca(promedio, distancia);
+if (resultado == true)
+{
+    Console.WriteLine("Felicidades,");
+    Console.WriteLine(nombre);
+    Console.WriteLine("ha sido seleccionado para recibir la beca.");
+}
+else
+{
+    Console.WriteLine("Lo sentimos,");
+    Console.WriteLine(nombre);
+    Console.WriteLine("no ha sido seleccionado para recibir la beca.");
+}
